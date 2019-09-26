b55d318c49fa866d5ade0df0945e1565c07143e4
fomenkoyegor <fomenkoyegor@gmail.com>
2019-09-26 19:37:41 +0300

    router-oulet fix mat header

--- src/app/components/mat-header/mat-header.component.ts
+++ src/app/components/mat-header/mat-header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {Component} from '@angular/core';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
+import {MarvelLinks} from '../../interfaces/marvel-links.enum';
 
 @Component({
   selector: 'app-mat-header',
@@ -9,13 +10,21 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./mat-header.component.scss']
 })
 export class MatHeaderComponent {
-
+  public links = [
+    MarvelLinks.characters,
+    MarvelLinks.comics,
+    MarvelLinks.creators,
+    MarvelLinks.events,
+    MarvelLinks.series,
+    MarvelLinks.stories,
+  ];
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
 }
